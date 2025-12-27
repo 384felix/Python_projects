a84dd11abc906243f1a23c1591cb0d7a201f24ff
384felix <fesil1@hotmail.de>
2025-12-27 23:27:38 +0100

    completed second tutorial and created folder structure

---
+++ YT_Tutorials/Tutorial_01.py
@@ -0,0 +1,14 @@
+greeting = 'Hello World!'
+print(greeting)  # Hello World is printed
+
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     WELCOME!     *"
+
+
+print()  # this is a comment
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
