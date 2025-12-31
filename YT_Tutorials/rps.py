c4d822802fbfc148256915e31dc4df9e7512095a
384felix <fesil1@hotmail.de>
2025-12-31 17:46:40 +0100

    Rock Paper Scissors mini game

---
+++ YT_Tutorials/rps.py
@@ -0,0 +1,47 @@
+import sys
+import random
+from enum import Enum
+
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+# print(RPS(2))               # RPS.PAPER
+# print(RPS.ROCK)             # RPS.ROCK
+# print(RPS['ROCK'])          # RPS.ROCK
+# print(RPS.ROCK.value)       # we get the number '1' back
+# sys.exit()
+
+print("")
+
+while 1 > 0:
+    playerchoice = input(
+        "Please enter ... \n1 for Rock\n2 for Paper, or \n3 for Scissors:\n\n")
+    player = int(playerchoice)
+
+    if player < 1 | player > 3:
+        sys.exit("You must enter 1, 2 or 3.")
+
+    computerchoice = random.choice("123")
+
+    computer = int(computerchoice)
+
+    print("")
+    print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
+    print("Python chose " + str(RPS(computer)).replace('RPS.', '') + ".")
+    print("")
+
+    if player == 1 and computer == 3:
+        print("You win! ")
+    elif player == 2 and computer == 1:
+        print("You win!")
+    elif player == 3 and computer == 2:
+        print("You win!")
+    elif player == computer:
+        print("Tie game!")
+    else:
+        print("Python wins!")
+    print("\n")
